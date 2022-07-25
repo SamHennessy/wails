da285072ed1f337a74eae5e95b0e2e80f7843267
Travis McLane <tmclane@gmail.com>
2022-07-25 14:57:07 -0500

    implement options.server + add to options.App

--- v2/pkg/options/options.go
+++ v2/pkg/options/options.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/wailsapp/wails/v2/pkg/options/linux"
 	"github.com/wailsapp/wails/v2/pkg/options/mac"
+	"github.com/wailsapp/wails/v2/pkg/options/server"
 	"github.com/wailsapp/wails/v2/pkg/options/windows"
 
 	"github.com/wailsapp/wails/v2/pkg/menu"
@@ -64,6 +65,7 @@ type App struct {
 	Windows *windows.Options
 	Mac     *mac.Options
 	Linux   *linux.Options
+	Server  *server.Options
 }
 
 type RGBA struct {
